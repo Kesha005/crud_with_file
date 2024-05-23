364152b74f473e50b8bbd07d3e722914572b70d7
Kesha005 <backendtm005@gmail.com>
2024-05-23 17:16:17 +0500

    client structure end

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	//"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
-
-
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -17,28 +16,56 @@ var upgrader = websocket.Upgrader{
 
 var clients []Client
 
+type Msgs struct {
+	To      int
+	Sender  int
+	Message string
+}
 
-
-type Client struct{
+type Client struct {
 	Conn websocket.Conn
 	Name string
 }
 
-
 func main() {
 	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil)
-		
+
+		defer conn.Close()
+
 		client_name := r.Header.Get("Client-Name")
-		if client_name ==""{
+		if client_name == "" {
 			panic("client name cannot be empty")
 
-			return 
+			return
 		}
 
-		client:= &Client{Name: client_name,Conn: *conn}
+		client := &Client{Name: client_name, Conn: *conn}
 
 		clients = append(clients, *client)
+
+		for {
+
+			var msg Msgs
+
+			msgType, message, err := conn.ReadMessage()
+			if err != nil {
+				panic(err)
+				break
+			}
+			if msgType == websocket.TextMessage {
+				if msgerr := json.Unmarshal(message, &msg); msgerr != nil {
+
+					continue
+				}
+
+			}
+
+			for _, client := range clients{
+				client.Conn.WriteMessage(msgType,message)
+			}
+
+		}
 
 	})
 
